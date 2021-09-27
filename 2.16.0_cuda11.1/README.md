4f48805108f635d2c04cd472fc9a9e44c6481326
Peter Reutemann <fracpete@gmail.com>
2021-09-27 16:19:14 +1300

    added CUDA 11.1 compatible version of 2.16.0

---
+++ 2.16.0_cuda11.1/README.md
@@ -0,0 +1,236 @@
+# MMDetection
+
+Allows processing of images with [MMDetection](https://github.com/open-mmlab/mmdetection).
+
+Uses PyTorch 1.9.0 and CUDA 11.1.
+
+## Version
+
+MMDetection github repo tag/hash:
+
+```
+v2.16.0
+7bd39044f35aec4b90dd797b965777541a8678ff
+```
+
+and timestamp:
+
+```
+August 31st, 2021
+```
+
+## Docker
+
+### Quick start
+
+* Log into registry using *public* credentials:
+
+  ```commandline
+  docker login -u public -p public public.aml-repo.cms.waikato.ac.nz:443 
+  ```
+
+* Pull and run image (adjust volume mappings `-v`):
+
+  ```commandline
+  docker run --gpus=all --shm-size 8G \
+    -v /local/dir:/container/dir \
+    -it public.aml-repo.cms.waikato.ac.nz:443/open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+
+  **NB:** For docker versions older than 19.03 (`docker version`), use `--runtime=nvidia` instead of `--gpus=all`.
+
+* If need be, remove all containers and images from your system:
+
+  ```commandline
+  docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker system prune -a
+  ```
+
+### Build local image
+
+* Build the image from Docker file (from within /path_to/mmdetection/2.16.0_cuda11,1)
+
+  ```commandline
+  docker build -t mmdet .
+  ```
+  
+* Run the container
+
+  ```commandline
+  docker run --gpus=all --shm-size 8G -v /local/dir:/container/dir -it mmdet
+  ```
+  `/local/dir:/container/dir` maps a local disk directory into a directory inside the container
+
+### Usage
+
+* Convert annotations (in ADAMS report format) to MS COCO JSON format using 
+  [wai.annotations](https://github.com/waikato-ufdl/wai-annotations). 
+  Conversion must be done twice, once for training set and again for validation set.
+  
+* Store class names or label strings in an environment variable called `MMDET_CLASSES` **(inside the container)**:
+
+  ```commandline
+  export MMDET_CLASSES=\'class1\',\'class2\',...
+  ```
+  
+* Alternatively, have the labels stored in a text file with the labels separated by commas and the `MMDET_CLASSES`
+  environment variable point at the file.
+  
+  * The labels are stored in `/data/labels.txt` as follows:
+
+    ```commandline
+    class1,class2,...
+    ``` 
+  
+  * Export `MMDET_CLASSES` as follows:
+
+    ```commandline
+    export MMDET_CLASSES=/data/labels.txt
+    ```
+
+* Copy the config file (of the model you want to train) from /mmdetection/configs (inside the container) or 
+  from [here](https://github.com/open-mmlab/mmdetection/tree/7bd39044f35aec4b90dd797b965777541a8678ff/configs) 
+  to local disk, then follow [these instructions](#config).
+
+* Train
+
+  ```commandline
+  mmdet_train /path_to/your_data_config.py
+  ```
+
+* Predict and produce CSV files
+
+  ```commandline
+  mmdet_predict --checkpoint /path_to/epoch_n.pth --config /path_to/your_data_config.py \
+    --prediction_in /path_to/test_imgs/ --prediction_out /path_to/test_results/ \
+    --labels /path_to/your_data/labels.txt --score 0
+  ```
+  Run with -h for all available options.
+
+  `--labels` is a text file with a comma-separated list of labels (excluding the background)
+
+  You may also need to specify the following options:
+
+  * `--mask_threshold` - if using another threshold than the default of 0.1
+  * `--mask_nth` - use every nth row/col of mask to speed up computation of polygon
+  * `--output_minrect`
+
+## Pre-built images
+
+* Build
+
+  ```commandline
+  docker build -t open-mmlab/mmdetection:2.16.0_cuda11,1 .
+  ```
+  
+* Tag
+
+  ```commandline
+  docker tag \
+    mmdetection:2.16.0_cuda11,1 \
+    public-push.aml-repo.cms.waikato.ac.nz:443/open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+  
+* Push
+
+  ```commandline
+  docker push public-push.aml-repo.cms.waikato.ac.nz:443/open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+  If error "no basic auth credentials" occurs, then run (enter username/password when prompted):
+  
+  ```commandline
+  docker login public-push.aml-repo.cms.waikato.ac.nz:443
+  ```
+  
+* Pull
+
+  If image is available in aml-repo and you just want to use it, you can pull using following command and then [run](#run).
+
+  ```commandline
+  docker pull public.aml-repo.cms.waikato.ac.nz:443/open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+  If error "no basic auth credentials" occurs, then run (enter username/password when prompted):
+  
+  ```commandline
+  docker login public.aml-repo.cms.waikato.ac.nz:443
+  ```
+  Then tag by running:
+  
+  ```commandline
+  docker tag \
+    public.aml-repo.cms.waikato.ac.nz:443/open-mmlab/mmdetection:2.16.0_cuda11,1 \
+    open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+  
+* <a name="run">Run</a>
+
+  ```commandline
+  docker run --gpus=all --shm-size 8G \
+    -v /local/dir:/container/dir -it open-mmlab/mmdetection:2.16.0_cuda11,1
+  ```
+  `/local/dir:/container/dir` maps a local disk directory into a directory inside the container
+
+
+## Example config files
+
+You can output example config files using:
+
+```commandline
+mmdet_config PATH_TO_CONFIG.py
+```
+
+You can browse the config files [here](https://github.com/open-mmlab/mmdetection/blob/v2.16.0/docs/model_zoo.md).
+
+
+## <a name="config">Preparing the config file</a>
+
+1. If necessary, change `num_classes` to labels + 1 (BG).
+2. In `train_cfg` and `test_cfg`: change `nms_pre`, `nms_post`, and `max_num` to the preferred values.
+3. Change `dataset_type` to `Dataset`
+4. Change `data_root` to the root path of your dataset (the directory containing train and val directories).
+5. Copy/paste `train_pipeline = [...]` and rename it to `val_pipeline`.
+6. Change `pipeline` for `val` to `val_pipeline`.
+7. In `train_pipeline`, `val_pipeline` and `test_pipeline`: change `img_scale` to preferred values. 
+   Image will be scaled to the smaller value between (larger_scale/larger_image_side) and (smaller_scale/smaller_image_side).
+8. Adapt `ann_file` and `img_prefix` to suit your dataset.
+9. Interval in `checkpoint_config` will determine the frequency of saving models while training 
+   (10 for example will save a model after every 10 epochs).
+10. Change `total_epochs` to how many epochs you want to train the model for.
+11. Change `work_dir` to the path where you want to save the trained models to.
+12. If you want to include the validation set, add `, ('val', 1)` to `workflow`.
+
+_You don't have to copy the config file back, just point at it when training._
+
+**NB:** A fully expanded config file will get placed in the output directory with the same
+name as the config plus the extension *.full*.
+
+
+## Permissions
+
+When running the docker container as regular use, you will want to set the correct
+user and group on the files generated by the container (aka the user:group launching
+the container):
+
+```commandline
+docker run -u $(id -u):$(id -g) -e USER=$USER ...
+```
+
+## Caching models
+
+PyTorch downloads base models, if necessary. However, by using Docker, this means that 
+models will get downloaded with each Docker image, using up unnecessary bandwidth and
+slowing down the startup. To avoid this, you can map a directory on the host machine
+to cache the base models for all processes (usually, there would be only one concurrent
+model being trained):  
+
+```
+-v /somewhere/local/cache:/.cache
+```
+
+Or specifically for PyTorch:
+
+```
+-v /somewhere/local/cache/torch:/.cache/torch
+```
+
+**NB:** When running the container as root rather than a specific user, the internal directory will have to be
+prefixed with `/root`. 
---
+++ 2.16.0_cuda11.1/predict.py
@@ -0,0 +1,320 @@
+import numpy as np
+import os
+import argparse
+from PIL import Image
+from image_complete import auto
+import torch
+import traceback
+
+import mmcv
+import pycocotools.mask as maskUtils
+from mmdet.apis import init_detector, inference_detector
+from sfp import Poller
+from wai.annotations.image_utils import image_to_numpyarray, remove_alpha_channel, mask_to_polygon, polygon_to_minrect, polygon_to_lists, lists_to_polygon, polygon_to_bbox
+from wai.annotations.core import ImageInfo
+from wai.annotations.roi import ROIObject
+from wai.annotations.roi.io import ROIWriter
+
+SUPPORTED_EXTS = [".jpg", ".jpeg", ".png", ".bmp"]
+""" supported file extensions (lower case). """
+
+
+def check_image(fname, poller):
+    """
+    Check method that ensures the image is valid.
+
+    :param fname: the file to check
+    :type fname: str
+    :param poller: the Poller instance that called the method
+    :type poller: Poller
+    :return: True if complete
+    :rtype: bool
+    """
+    result = auto.is_image_complete(fname)
+    poller.debug("Image complete:", fname, "->", result)
+    return result
+
+
+def process_image(fname, output_dir, poller):
+    """
+    Method for processing an image.
+
+    :param fname: the image to process
+    :type fname: str
+    :param output_dir: the directory to write the image to
+    :type output_dir: str
+    :param poller: the Poller instance that called the method
+    :type poller: Poller
+    :return: the list of generated output files
+    :rtype: list
+    """
+    result = []
+
+    try:
+        image = Image.open(fname)
+        image = remove_alpha_channel(image)
+        image_array = image_to_numpyarray(image)
+        detection = inference_detector(model, image_array)
+
+        assert isinstance(poller.params.class_names, (tuple, list))
+        if isinstance(detection, tuple):
+            bbox_result, segm_result = detection
+            if isinstance(segm_result, tuple):
+                segm_result = segm_result[0]  # ms rcnn
+        else:
+            bbox_result, segm_result = detection, None
+        bboxes = np.vstack(bbox_result)
+        labels = [
+            np.full(bbox.shape[0], i, dtype=np.int32)
+            for i, bbox in enumerate(bbox_result)
+        ]
+        labels = np.concatenate(labels)
+
+        roi_path = "{}/{}-rois.csv".format(output_dir, os.path.splitext(os.path.basename(fname))[0])
+        img_path = "{}/{}-mask.png".format(output_dir, os.path.splitext(os.path.basename(fname))[0])
+
+        # rois
+        roiobjs = []
+        mask_comb = None
+        for index in range(len(bboxes)):
+            x0, y0, x1, y1, score = bboxes[index]
+            label = labels[index]
+            label_str = poller.params.class_names[label]
+
+            # Ignore this roi if the score is less than the provided threshold
+            if score < poller.params.score_threshold:
+                continue
+
+            # Translate roi coordinates into original image coordinates (before combining)
+            x0n = x0 / image.width
+            y0n = y0 / image.height
+            x1n = x1 / image.width
+            y1n = y1 / image.height
+
+            px = None
+            py = None
+            pxn = None
+            pyn = None
+            bw = None
+            bh = None
+
+            if segm_result is not None:
+                px = []
+                py = []
+                pxn = []
+                pyn = []
+                bw = ""
+                bh = ""
+
+                segms = mmcv.concat_list(segm_result)
+                if isinstance(segms[0], torch.Tensor):
+                    mask = torch.stack(segms, dim=0).detach().cpu().numpy()
+                else:
+                    mask = np.stack(segms, axis=0)
+                mask = maskUtils.decode(mask).astype(np.int)
+                poly = mask_to_polygon(mask, poller.params.mask_threshold, mask_nth=poller.params.mask_nth, view=(x0, y0, x1, y1),
+                                       view_margin=poller.params.view_margin, fully_connected=poller.params.fully_connected)
+                if len(poly) > 0:
+                    px, py = polygon_to_lists(poly[0], swap_x_y=True, normalize=False)
+                    pxn, pyn = polygon_to_lists(poly[0], swap_x_y=True, normalize=True, img_width=image.width, img_height=image.height)
+                    if poller.params.output_minrect:
+                        bw, bh = polygon_to_minrect(poly[0])
+                    if poller.params.bbox_as_fallback >= 0:
+                        if len(px) >= 3:
+                            p_x0n, p_y0n, p_x1n, p_y1n = polygon_to_bbox(lists_to_polygon(pxn, pyn))
+                            p_area = (p_x1n - p_x0n) * (p_y1n - p_y0n)
+                            b_area = (x1n - x0n) * (y1n - y0n)
+                            if (b_area > 0) and (p_area / b_area < poller.params.bbox_as_fallback):
+                                px = [float(i) for i in [x0, x1, x1, x0]]
+                                py = [float(i) for i in [y0, y0, y1, y1]]
+                                pxn = [float(i) for i in [x0n, x1n, x1n, x0n]]
+                                pyn = [float(i) for i in [y0n, y0n, y1n, y1n]]
+                        else:
+                            px = [float(i) for i in [x0, x1, x1, x0]]
+                            py = [float(i) for i in [y0, y0, y1, y1]]
+                            pxn = [float(i) for i in [x0n, x1n, x1n, x0n]]
+                            pyn = [float(i) for i in [y0n, y0n, y1n, y1n]]
+                        if poller.params.output_minrect:
+                            bw = x1 - x0 + 1
+                            bh = y1 - y0 + 1
+                    if poller.params.fit_bbox_to_polygon:
+                        if len(px) >= 3:
+                            x0, y0, x1, y1 = polygon_to_bbox(lists_to_polygon(px, py))
+                            x0n, y0n, x1n, y1n = polygon_to_bbox(lists_to_polygon(pxn, pyn))
+
+                if poller.params.output_mask_image:
+                    mask_img = mask.copy()
+                    mask_img[mask_img < poller.params.mask_threshold] = 0
+                    mask_img[mask_img >= poller.params.mask_threshold] = label+1  # first label is 0
+                    if mask_comb is None:
+                        mask_comb = mask_img
+                    else:
+                        tmp = np.where(mask_comb==0, mask_img, mask_comb)
+                        mask_comb = tmp
+
+            roiobj = ROIObject(x0, y0, x1, y1, x0n, y0n, x1n, y1n, label, label_str, score=score,
+                               poly_x=px, poly_y=py, poly_xn=pxn, poly_yn=pyn,
+                               minrect_w=bw, minrect_h=bh)
+            roiobjs.append(roiobj)
+
+        info = ImageInfo(os.path.basename(fname))
+        roiext = (info, roiobjs)
+        options = ["--output", output_dir, "--no-images"]
+        if poller.params.output_width_height:
+            options.append("--size-mode")
+        roiwriter = ROIWriter(options)
+        roiwriter.save([roiext])
+        result.append(roi_path)
+
+        if mask_comb is not None:
+            im = Image.fromarray(np.uint8(mask_comb), 'P')
+            im.save(img_path, "PNG")
+            result.append(img_path)
+    except KeyboardInterrupt:
+        poller.keyboard_interrupt()
+    except:
+        poller.error("Failed to process image: %s\n%s" % (fname, traceback.format_exc()))
+    return result
+
+
+def predict_on_images(input_dir, model, output_dir, tmp_dir, class_names, score_threshold=0.0,
+                      poll_wait=1.0, continuous=False, use_watchdog=False, watchdog_check_interval=10.0,
+                      delete_input=False, mask_threshold=0.1, mask_nth=1,
+                      output_minrect=False, view_margin=2, fully_connected='high', fit_bbox_to_polygon=False,
+                      output_width_height=False, bbox_as_fallback=-1.0, output_mask_image=False,
+                      verbose=False, quiet=False):
+    """
+    Method for performing predictions on images.
+
+    :param input_dir: the directory with the images
+    :type input_dir: str
+    :param model: the mmdetection trained model
+    :param output_dir: the output directory to move the images to and store the predictions
+    :type output_dir: str
+    :param tmp_dir: the temporary directory to store the predictions until finished, use None if not to use
+    :type tmp_dir: str
+    :param class_names: labels or class names
+    :type class_names: list[str]
+    :param score_threshold: the minimum score predictions have to have
+    :type score_threshold: float
+    :param poll_wait: the amount of seconds between polls when not in watchdog mode
+    :type poll_wait: float
+    :param continuous: whether to poll continuously
+    :type continuous: bool
+    :param use_watchdog: whether to react to file creation events rather than use fixed-interval polling
+    :type use_watchdog: bool
+    :param watchdog_check_interval: the interval for the watchdog process to check for files that were missed due to potential race conditions
+    :type watchdog_check_interval: float
+    :param delete_input: whether to delete the input images rather than moving them to the output directory
+    :type delete_input: bool
+    :param mask_threshold: the threshold to use for determining the contour of a mask
+    :type mask_threshold: float
+    :param mask_nth: to speed up polygon computation, use only every nth row and column from mask
+    :type mask_nth: int
+    :param output_minrect: when predicting polygons, whether to output the minimal rectangles around the objects as well
+    :type output_minrect: bool
+    :param view_margin: the margin in pixels to use around the masks
+    :type view_margin: int
+    :param fully_connected: whether regions of 'high' or 'low' values should be fully-connected at isthmuses
+    :type fully_connected: str
+    :param fit_bbox_to_polygon: whether to fit the bounding box to the polygon
+    :type fit_bbox_to_polygon: bool
+    :param output_width_height: whether to output x/y/w/h instead of x0/y0/x1/y1
+    :type output_width_height: bool
+    :param bbox_as_fallback: if ratio between polygon-bbox and bbox is smaller than this value, use bbox as fallback polygon, ignored if < 0
+    :type bbox_as_fallback: float
+    :param output_mask_image: when generating masks, whether to output a combined mask image as well
+    :type output_mask_image: bool
+    :param verbose: whether to output more logging information
+    :type verbose: bool
+    :param quiet: whether to suppress output
+    :type quiet: bool
+    """
+
+    if fit_bbox_to_polygon and (bbox_as_fallback >= 0):
+        raise Exception("fit_bbox_to_polygon and bbox_as_fallback cannot be used together!")
+
+    poller = Poller()
+    poller.model = model
+    poller.input_dir = input_dir
+    poller.output_dir = output_dir
+    poller.tmp_dir = tmp_dir
+    poller.extensions = SUPPORTED_EXTS
+    poller.delete_input = delete_input
+    poller.progress = not quiet
+    poller.verbose = verbose
+    poller.check_file = check_image
+    poller.process_file = process_image
+    poller.poll_wait = poll_wait
+    poller.continuous = continuous
+    poller.use_watchdog = use_watchdog
+    poller.watchdog_check_interval = watchdog_check_interval
+    poller.params.class_names = class_names
+    poller.params.score_threshold = score_threshold
+    poller.params.mask_threshold = mask_threshold
+    poller.params.mask_nth = mask_nth
+    poller.params.view_margin = view_margin
+    poller.params.fully_connected = fully_connected
+    poller.params.output_minrect = output_minrect
+    poller.params.bbox_as_fallback = bbox_as_fallback
+    poller.params.fit_bbox_to_polygon = fit_bbox_to_polygon
+    poller.params.output_mask_image = output_mask_image
+    poller.params.output_width_height = output_width_height
+    poller.poll()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint', help='Path to the trained model checkpoint', required=True, default=None)
+    parser.add_argument('--config', help='Path to the config file', required=True, default=None)
+    parser.add_argument('--prediction_in', help='Path to the test images', required=True, default=None)
+    parser.add_argument('--prediction_out', help='Path to the output csv files folder', required=True, default=None)
+    parser.add_argument('--prediction_tmp', help='Path to the temporary csv files folder', required=False, default=None)
+    parser.add_argument('--labels', help='Path to text file with comma seperated labels', required=True, default=None)
+    parser.add_argument('--score', type=float, help='Score threshold to include in csv file', required=False, default=0.0)
+    parser.add_argument('--mask_threshold', type=float, help='The threshold (0-1) to use for determining the contour of a mask', required=False, default=0.1)
+    parser.add_argument('--mask_nth', type=int, help='To speed polygon detection up, use every nth row and column only', required=False, default=1)
+    parser.add_argument('--output_minrect', action='store_true', help='When outputting polygons whether to store the minimal rectangle around the objects in the CSV files as well', required=False, default=False)
+    parser.add_argument('--fit_bbox_to_polygon', action='store_true', help='Whether to fit the bounding box to the polygon', required=False, default=False)
+    parser.add_argument('--bbox_as_fallback', default=-1.0, type=float,
+                        help='When outputting polygons the bbox can be used as fallback polygon. This happens if the ratio '
+                             + 'between the surrounding bbox of the polygon and the bbox is smaller than the specified value. '
+                             + 'Turned off if < 0.', required=False)
+    parser.add_argument('--poll_wait', type=float, help='poll interval in seconds when not using watchdog mode', required=False, default=1.0)
+    parser.add_argument('--continuous', action='store_true', help='Whether to continuously load test images and perform prediction', required=False, default=False)
+    parser.add_argument('--use_watchdog', action='store_true', help='Whether to react to file creation events rather than performing fixed-interval polling', required=False, default=False)
+    parser.add_argument('--watchdog_check_interval', type=float, help='check interval in seconds for the watchdog', required=False, default=10.0)
+    parser.add_argument('--delete_input', action='store_true', help='Whether to delete the input images rather than move them to --prediction_out directory', required=False, default=False)
+    parser.add_argument('--view_margin', default=2, type=int, required=False, help='The number of pixels to use as margin around the masks when determining the polygon')
+    parser.add_argument('--fully_connected', default='high', choices=['high', 'low'], required=False, help='When determining polygons, whether regions of high or low values should be fully-connected at isthmuses')
+    parser.add_argument('--output_width_height', action='store_true', help="Whether to output x/y/w/h instead of x0/y0/x1/y1 in the ROI CSV files", required=False, default=False)
+    parser.add_argument('--output_mask_image', action='store_true', help="Whether to output a mask image (PNG) when predictions generate masks", required=False, default=False)
+    parser.add_argument('--verbose', action='store_true', help='Whether to output more logging info', required=False, default=False)
+    parser.add_argument('--quiet', action='store_true', help='Whether to suppress output', required=False, default=False)
+    parsed = parser.parse_args()
+
+    if parsed.fit_bbox_to_polygon and (parsed.bbox_as_fallback >= 0):
+        raise Exception("Options --fit_bbox_to_polygon and --bbox_as_fallback cannot be used together!")
+
+    try:
+        # This is the actual model that is used for the object detection
+        model = init_detector(parsed.config, parsed.checkpoint)
+        
+        # Get class names
+        with open(parsed.labels, "r") as labels_file:
+            class_names = labels_file.read().strip()
+            class_names = class_names.split(",")
+
+        # Performing the prediction and producing the csv files
+        predict_on_images(parsed.prediction_in, model, parsed.prediction_out, parsed.prediction_tmp, class_names,
+                          score_threshold=parsed.score, continuous=parsed.continuous,
+                          use_watchdog=parsed.use_watchdog, watchdog_check_interval=parsed.watchdog_check_interval,
+                          delete_input=parsed.delete_input,
+                          mask_threshold=parsed.mask_threshold, mask_nth=parsed.mask_nth,
+                          output_minrect=parsed.output_minrect, view_margin=parsed.view_margin,
+                          fully_connected=parsed.fully_connected, fit_bbox_to_polygon=parsed.fit_bbox_to_polygon,
+                          output_width_height=parsed.output_width_height, bbox_as_fallback=parsed.bbox_as_fallback,
+                          output_mask_image=parsed.output_mask_image, verbose=parsed.verbose, quiet=parsed.quiet)
+
+    except Exception as e:
+        print(traceback.format_exc())
