1f3d9fa0fdc2d42d1d5aa291af6b67e1c29a04ab
Peter Reutemann <fracpete@gmail.com>
2023-09-01 15:57:27 +1200

    updated version of script

--- 3.1.0_cpu/predict_redis.py
+++ 3.1.0_cpu/predict_redis.py
@@ -1,14 +1,13 @@
 from datetime import datetime
 import io
-import numpy as np
 from PIL import Image
 import torch
 import traceback
 
-import mmcv
 from mmdet.apis import init_detector, inference_detector
 from mmdet.datasets.ext_dataset import determine_classes
-from wai.annotations.image_utils import image_to_numpyarray, remove_alpha_channel, mask_to_polygon, polygon_to_minrect, polygon_to_lists, lists_to_polygon, polygon_to_bbox
+from mmdet.structures.mask import BitmapMasks, PolygonMasks, bitmap_to_polygon
+from wai.annotations.image_utils import image_to_numpyarray, remove_alpha_channel, polygon_to_minrect, polygon_to_lists, lists_to_polygon, polygon_to_bbox
 from rdh import Container, MessageContainer, create_parser, configure_redis, run_harness, log
 from opex import ObjectPredictions, ObjectPrediction, Polygon, BBox
 
@@ -28,37 +27,38 @@ def process_image(msg_cont):
         image = remove_alpha_channel(image)
         image_array = image_to_numpyarray(image)
         detection = inference_detector(model, image_array)
+        if 'pred_instances' in detection:
+            pred_instances = detection.pred_instances
+            pred_instances = pred_instances[pred_instances.scores > config.score_threshold]
+        else:
+            log("No 'pred_instances' in detection data for: %s" % str(start_time))
+            return
 
         assert isinstance(config.class_names, (tuple, list))
-        if isinstance(detection, tuple):
-            bbox_result, segm_result = detection
-            if isinstance(segm_result, tuple):
-                segm_result = segm_result[0]  # ms rcnn
-        else:
-            bbox_result, segm_result = detection, None
-        bboxes = np.vstack(bbox_result)
-        labels = [
-            np.full(bbox.shape[0], i, dtype=np.int32)
-            for i, bbox in enumerate(bbox_result)
-        ]
-        labels = np.concatenate(labels)
+        labels = pred_instances.labels
+        scores = pred_instances.scores
+        bboxes = None
         masks = None
-        if segm_result is not None:
-            segms = mmcv.concat_list(segm_result)
-            if isinstance(segms[0], torch.Tensor):
-                masks = torch.stack(segms, dim=0).detach().cpu().numpy()
-            else:
-                masks = np.stack(segms, axis=0)
+        if 'bboxes' in pred_instances:
+            bboxes = pred_instances.bboxes
+        if 'masks' in pred_instances:
+            masks = pred_instances.masks
+            if isinstance(masks, torch.Tensor):
+                masks = masks.cpu().numpy()
+            elif isinstance(masks, (PolygonMasks, BitmapMasks)):
+                masks = masks.to_ndarray()
+            masks = masks.astype(bool)
 
         objs = []
         for index in range(len(bboxes)):
-            x0, y0, x1, y1, score = bboxes[index]
-            label = labels[index]
+            x0, y0, x1, y1 = bboxes[index]
+            x0 = float(x0)
+            y0 = float(y0)
+            x1 = float(x1)
+            y1 = float(y1)
+            score = float(scores[index])
+            label = int(labels[index])
             label_str = config.class_names[label]
-
-            # Ignore this roi if the score is less than the provided threshold
-            if score < config.score_threshold:
-                continue
 
             # Translate roi coordinates into original image coordinates (before combining)
             x0n = x0 / image.width
@@ -69,15 +69,13 @@ def process_image(msg_cont):
             px = None
             py = None
 
-            if segm_result is not None:
+            if masks is not None:
                 px = []
                 py = []
-                mask = masks[index].astype(bool)
-                poly = mask_to_polygon(mask, config.mask_threshold, mask_nth=config.mask_nth, view=(x0, y0, x1, y1),
-                                       view_margin=config.view_margin, fully_connected=config.fully_connected)
+                poly, _ = bitmap_to_polygon(masks[index])
                 if len(poly) > 0:
-                    px, py = polygon_to_lists(poly[0], swap_x_y=True, normalize=False)
-                    pxn, pyn = polygon_to_lists(poly[0], swap_x_y=True, normalize=True, img_width=image.width, img_height=image.height)
+                    px, py = polygon_to_lists(poly[0], swap_x_y=False, normalize=False)
+                    pxn, pyn = polygon_to_lists(poly[0], swap_x_y=False, normalize=True, img_width=image.width, img_height=image.height)
                     if config.bbox_as_fallback >= 0:
                         if len(px) >= 3:
                             p_x0n, p_y0n, p_x1n, p_y1n = polygon_to_bbox(lists_to_polygon(pxn, pyn))
@@ -123,17 +121,12 @@ if __name__ == '__main__':
     parser = create_parser('MMDetection - Prediction (Redis)', prog="mmdet_predict_redis", prefix="redis_")
     parser.add_argument('--checkpoint', help='Path to the trained model checkpoint', required=True, default=None)
     parser.add_argument('--config', help='Path to the config file', required=True, default=None)
-    parser.add_argument('--labels', help='ignored, use MMDET_CLASSES environment variable', required=False, default=None)
     parser.add_argument('--score', type=float, help='Score threshold to include in csv file', required=False, default=0.0)
-    parser.add_argument('--mask_threshold', type=float, help='The threshold (0-1) to use for determining the contour of a mask', required=False, default=0.1)
-    parser.add_argument('--mask_nth', type=int, help='To speed polygon detection up, use every nth row and column only', required=False, default=1)
     parser.add_argument('--fit_bbox_to_polygon', action='store_true', help='Whether to fit the bounding box to the polygon', required=False, default=False)
     parser.add_argument('--bbox_as_fallback', default=-1.0, type=float,
                         help='When outputting polygons the bbox can be used as fallback polygon. This happens if the ratio '
                              + 'between the surrounding bbox of the polygon and the bbox is smaller than the specified value. '
                              + 'Turned off if < 0.', required=False)
-    parser.add_argument('--view_margin', default=2, type=int, required=False, help='The number of pixels to use as margin around the masks when determining the polygon')
-    parser.add_argument('--fully_connected', default='high', choices=['high', 'low'], required=False, help='When determining polygons, whether regions of high or low values should be fully-connected at isthmuses')
     parser.add_argument('--verbose', action='store_true', help='Whether to output more logging info', required=False, default=False)
     parsed = parser.parse_args()
 
@@ -153,10 +146,6 @@ if __name__ == '__main__':
         config.class_names = class_names
         config.model = model
         config.score_threshold = parsed.score
-        config.mask_threshold = parsed.mask_threshold
-        config.mask_nth = parsed.mask_nth
-        config.view_margin = parsed.view_margin
-        config.fully_connected = parsed.fully_connected
         config.fit_bbox_to_polygon = parsed.fit_bbox_to_polygon
         config.bbox_as_fallback = parsed.bbox_as_fallback
         config.verbose = parsed.verbose
